ede13ab89b4743113a0b88f58ad646b6e97e7819
zzp <zzp13369137498@163.com>
2024-02-02 22:18:49 +0800

    second commit

--- src/main/java/com/yupi/usercenter/UserCenterApplication.java
+++ src/main/java/com/yupi/usercenter/UserCenterApplication.java
@@ -9,5 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserCenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserCenterApplication.class, args);
+        System.out.println("second");
     }
 }
